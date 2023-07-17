84feefbeb83be2b9f0d6c12a00df46e52a4e2866
shaotj <shaotj@300624.cn>
2023-07-17 10:55:30 +0800

    add 路由

--- lavarel-demo/routes/web.php
+++ lavarel-demo/routes/web.php
@@ -16,3 +16,120 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/hello", function () {
+    return "php是最好的语言~";
+});
+
+//Route::get("/user","UserController@index");
+/**
+ * 路由参数,必填参数   路由的参数通常都会被放在 {} 内，并且参数名只能为字母
+ * http://localhost:8000/user/1000
+ */
+Route::get('user/{id}', function ($id) {
+    return "我想要" . $id . "个亿";
+});
+
+/**
+ * 也可以根据需要在路由中定义多个参数：
+ * http://localhost:8000/posts/10/comments/24
+ */
+
+Route::get("/posts/{post}/comments/{comment}", function ($post, $commentId) {
+    return "创意是最好的厨师" . $post . $commentId;
+});
+/**
+ * 可选参数:在参数后加？，但前提是要确保路由的相应变量有默认值：
+ *
+ */
+Route::get("user1/{name?}",function ($name=null){
+   return "可选参数:".$name;
+});
+
+Route::get('user1/{name?}', function ($name = 'John') {
+    return "可选参数:".$name;
+});
+/**
+ * 编码正斜杠字符#
+ * http://localhost:8000/search/1
+ */
+Route::get("search/{search}",function ($search){
+    return $search;
+})->where("search",".");
+/**
+ * http://localhost:8000/user/profile
+ */
+Route::get('user/profile', function () {
+    //
+})->name('profile');
+
+
+
+
+/**
+ * 路由组
+ */
+Route::middleware(["first","second"])->group(function (){
+    Route::get("/",function (){
+
+    });
+    Route::get("/user/profile",function (){
+
+    });
+});
+
+/**
+ * 命名空间
+ */
+Route::namespace("admin")->group(function (){
+
+});
+
+/**
+ * 子域名路由
+ */
+Route::domain("{account}.myapp.com")->group(function (){
+    Route::get("user/{id}",function ($account,$id){
+        //
+    });
+});
+/**
+ * 路由前缀
+ * http://localhost:8000/admin/users/
+ */
+Route::prefix("admin")->group(function (){
+    Route::get("users",function (){
+      // 匹配包含 「/admin/users」 的 URL
+        return "我是路由组~~~";
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
